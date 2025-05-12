871bf907c12d6e57e38d4c2d974e06d8bc2c244e
GetecOffice <getec.plc@gmail.com>
2025-05-12 00:22:05 -0600

    settings

--- sensores/settings.py
+++ sensores/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Aplicacion',
 ]
-
+CSRF_TRUSTED_ORIGINS = ['https://tostador-production.up.railway.app']
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
